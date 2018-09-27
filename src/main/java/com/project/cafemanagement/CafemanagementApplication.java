95886122e2c8d047e723d4574609092cee4b92fa
nhut huynh <nhut.huynh@gmail.com>
2018-09-27 13:46:54 +1000

    init the project

---
+++ src/main/java/com/project/cafemanagement/CafemanagementApplication.java
@@ -0,0 +1,12 @@
+package com.project.cafemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafemanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafemanagementApplication.class, args);
+	}
+}
