486c0cf14d46a5ff221a4deca4c8d210d56d6900
nhut huynh <nhut.huynh@gmail.com>
2019-05-13 14:03:14 +1000

    add query findByFromDate to check duplicate fromDate

--- src/main/java/com/project/cafeemployeemanagement/repository/LeaveRequestRepository.java
+++ src/main/java/com/project/cafeemployeemanagement/repository/LeaveRequestRepository.java
@@ -2,21 +2,17 @@ package com.project.cafeemployeemanagement.repository;
 
 import com.project.cafeemployeemanagement.model.LeaveRequest;
 import com.project.cafeemployeemanagement.model.LeaveStatus;
+import com.project.cafeemployeemanagement.model.RoleName;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
 import java.util.List;
 
 @Repository
 public interface LeaveRequestRepository extends JpaRepository<LeaveRequest, Long> {
-
-    @Query("SELECT l FROM LeaveRequest as l WHERE l.status = :status")
-    List<LeaveRequest> findByStatus(@Param("status") LeaveStatus status);
-
-    @Query("SELECT l FROM LeaveRequest as l WHERE l.updatedManagerId = :managerId")
-    List<LeaveRequest> findByUpdatedManagerId(@Param("managerId") Long managerId);
 
     @Query("SELECT l FROM LeaveRequest as l WHERE l.employee.id = :employeeId")
     List<LeaveRequest> findByEmployee(@Param("employeeId") long employeeId);
@@ -26,4 +22,9 @@ public interface LeaveRequestRepository extends JpaRepository<LeaveRequest, Long
 
     @Query("SELECT l FROM LeaveRequest as l WHERE l.employee.shopOwnerId = :shopOwnerId and l.status = :status")
     List<LeaveRequest> findByShopOwnerIdAndStatus(@Param("shopOwnerId") long shopOwnerId, @Param("status") LeaveStatus status);
+
+    @Query("SELECT l FROM LeaveRequest as l WHERE l.employee.shopOwnerId = :shopOwnerId and l.employee.role.name = :roleName  and l.status = :status")
+    List<LeaveRequest> findByShopOwnerIdAndStatusAndIsEmployeeRole(@Param("shopOwnerId") long shopOwnerId, @Param("roleName") RoleName roleName, @Param("status") LeaveStatus status);
+
+    LeaveRequest findByFromDate(Date fromDate);
 }
