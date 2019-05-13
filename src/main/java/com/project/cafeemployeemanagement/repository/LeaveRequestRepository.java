826174a28786c0ad6f6cbf09eff7c60ac665e4b8
nhut huynh <nhut.huynh@gmail.com>
2019-05-13 15:00:32 +1000

    update query find submitted leave request whether within ones

--- src/main/java/com/project/cafeemployeemanagement/repository/LeaveRequestRepository.java
+++ src/main/java/com/project/cafeemployeemanagement/repository/LeaveRequestRepository.java
@@ -18,13 +18,14 @@ public interface LeaveRequestRepository extends JpaRepository<LeaveRequest, Long
     List<LeaveRequest> findByEmployee(@Param("employeeId") long employeeId);
 
     @Query("SELECT l FROM LeaveRequest as l WHERE l.employee.id = :employeeId and l.status = :status")
-    List<LeaveRequest> findByEmployeeAndStatus(@Param("employeeId") Long employeeId, @Param("status") LeaveStatus status);
+    List<LeaveRequest> findByEmployeeAndStatusOrderByCreatedDateAsc(@Param("employeeId") Long employeeId, @Param("status") LeaveStatus status);
 
     @Query("SELECT l FROM LeaveRequest as l WHERE l.employee.shopOwnerId = :shopOwnerId and l.status = :status")
-    List<LeaveRequest> findByShopOwnerIdAndStatus(@Param("shopOwnerId") long shopOwnerId, @Param("status") LeaveStatus status);
+    List<LeaveRequest> findByShopOwnerIdAndStatusOrderByCreatedDateDesc(@Param("shopOwnerId") long shopOwnerId, @Param("status") LeaveStatus status);
 
     @Query("SELECT l FROM LeaveRequest as l WHERE l.employee.shopOwnerId = :shopOwnerId and l.employee.role.name = :roleName  and l.status = :status")
-    List<LeaveRequest> findByShopOwnerIdAndStatusAndIsEmployeeRole(@Param("shopOwnerId") long shopOwnerId, @Param("roleName") RoleName roleName, @Param("status") LeaveStatus status);
+    List<LeaveRequest> findByShopOwnerIdAndStatusAndIsEmployeeRoleOrderByCreatedDateDesc(@Param("shopOwnerId") long shopOwnerId, @Param("roleName") RoleName roleName, @Param("status") LeaveStatus status);
 
-    LeaveRequest findByFromDate(Date fromDate);
+    @Query("SELECT l FROM LeaveRequest as l WHERE l.employee.id = :employeeId and l.fromDate <= :fromDate and l.toDate >= :toDate and l.status <> :status")
+    LeaveRequest findByEmployeeAndWithInFromDateAndToDateAndIsNotStatus(@Param("employeeId") long employeeId, @Param("fromDate") Date fromDate, @Param("toDate") Date toDate, @Param("status") LeaveStatus status);
 }
